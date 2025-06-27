33087e61b59501e6a97760da52452fce3b5f8fc3
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2025-06-27 23:20:09 +0600

    feat: Add team component and related styles, update configuration and dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,20 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "images.pexels.com",
+				port: "",
+				pathname: "/**",
+			},
+		],
+	},
 };
 
 export default withMDX(config);
---
+++ src/app/(home)/about/page.tsx
@@ -0,0 +1,12 @@
+import Team05Page from "@/components/team-05/team-05";
+import React from "react";
+
+const AboutPage = () => {
+	return (
+		<main>
+			<Team05Page />
+		</main>
+	);
+};
+
+export default AboutPage;
